d0090cf5c8006431b6e93c68dfadc7db9f6b68e4
sergio rodas <oscar.d.rodas@gmail.com>
2022-05-09 22:20:26 -0400

    completed view and controller the guides

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,2 +1,43 @@
 class GuidesController < ApplicationController
+  before_action :set_place, only: [:show, :new, :create]
+
+  def index
+    @guides = Guide.all
+  end
+
+  def show
+    @guide = Guide.find(params[:id])
+    @guide.place = @place
+  end
+
+  def new
+    @guide = Guide.new
+  end
+
+  def create
+    @guide = Guide.new(guide_params)
+    @guide.place = @place
+    if @guide.save
+      redirect_to guide_path(@guide)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @guide = Guide.find(params[:id])
+    @guide.destroy
+
+    redirect_to guides_path
+  end
+
+  private
+
+  def guide_params
+    params.require(:guide).permit(:first_name, :last_name, :phone_number, :guide_name, :dni, :email, :date_of_birth, :country, :city, :age)
+  end
+
+  def set_place
+    @place = Place.find(params[:place_id])
+  end
 end
--- app/models/guide.rb
+++ app/models/guide.rb
@@ -1,9 +1,7 @@
 class Guide < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
   has_many :reviews, dependent: :destroy
   has_many :bookings, dependent: :destroy
+  has_many :places, through: :bookings
+
+  has_one_attached :photo
 end
